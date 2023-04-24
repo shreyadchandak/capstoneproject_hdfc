898c522488ba32fb5cf808307995f3f6cb3dcae4
shreyadchandak <shreyachandak16@gmail.com>
2023-04-24 12:03:03 +0530

    Performed Encryption and Decryption at the webservice and Client Side

--- capstone-employee-project-client/src/main/java/com/hdfc/capstone/employee/client/controller/EmployeeController.java
+++ capstone-employee-project-client/src/main/java/com/hdfc/capstone/employee/client/controller/EmployeeController.java
@@ -1,7 +1,9 @@
 package com.hdfc.capstone.employee.client.controller;
 
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +12,9 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import com.hdfc.capstone.employee.client.dto.EmployeeDTO;
+import com.hdfc.capstone.employee.client.dto.EmployeeDTOEncrypted;
 import com.hdfc.capstone.employee.client.exception.InvalidEmployeeIdException;
+import com.hdfc.capstone.employee.client.utils.AESUtils;
 
 @RestController
 @RequestMapping("/employee")
@@ -22,17 +26,29 @@ public class EmployeeController {
 	String baseUrl="https://localhost:8080/employee";
 	
 
+	EmployeeDTOEncrypted response;
 	
 	@GetMapping("/getById/{employeeId}")
-	public ResponseEntity<EmployeeDTO> getById(@PathVariable long employeeId) throws InvalidEmployeeIdException{
+	public  EmployeeDTO getById(@PathVariable long employeeId) throws Exception,InvalidEmployeeIdException{
 		try {
-		ResponseEntity<EmployeeDTO> response = restTemplate.getForEntity(baseUrl+"/getById/"+employeeId, EmployeeDTO.class);
-
-		return response;
+	    response = restTemplate.getForObject(baseUrl+"/getById/"+employeeId, EmployeeDTOEncrypted.class);
 	}catch (HttpClientErrorException.BadRequest ex) {
 		
 		throw new InvalidEmployeeIdException();
+	}		
+		return toDTO(response);
+
+	
 	}
 	
+	public EmployeeDTO toDTO(EmployeeDTOEncrypted employeeEncrypted) throws Exception {
+		EmployeeDTO employeeDTO=new EmployeeDTO();
+        byte[] decryptedDateOfBirth = AESUtils.decrypt(employeeEncrypted.getDateOfBirth());
+	    LocalDate date= LocalDate.parse(new String(decryptedDateOfBirth, StandardCharsets.UTF_8));
+	    employeeDTO.setEmployeeName(employeeEncrypted.getEmployeeName());
+	    employeeDTO.setEmployeeId(employeeEncrypted.getEmployeeId());
+	    employeeDTO.setDateOfBirth(date);
+	    return employeeDTO;
+
 	}
 }
